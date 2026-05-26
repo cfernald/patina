5fcbd8755b34c6c546e749b48d3843bcdcb9d5ec
Chris Fernald <chfernal@microsoft.com>
2026-05-26 10:55:26 -0700

    patina_dxe_core: Fix test check for new goblin
    The updated goblin added some internal checks for PE
    consistency that is failing for aarch64 with our test
    binary. This commit updates the check to allow for either
    the patina or the goblin failures to be accepted.

--- patina_dxe_core/src/pi_dispatcher/image.rs
+++ patina_dxe_core/src/pi_dispatcher/image.rs
@@ -1803,7 +1803,13 @@ mod tests {
                 core::ptr::null_mut(),
                 Some(image.as_slice()),
             );
-            assert!(matches!(status, Err(ImageStatus::LoadError(EfiError::LoadError))));
+            // Check for either load error or unsupported. On aarch64, goblin will report parse
+            // the resources as well and fail due to the invalidate directory table size and
+            // unsupported will be returned.
+            assert!(matches!(
+                status,
+                Err(ImageStatus::LoadError(EfiError::LoadError)) | Err(ImageStatus::LoadError(EfiError::Unsupported))
+            ));
         });
     }
 
